2d8ac23c280a6c511fcb1fb53818c0a24ce7a9ac
Anna <apayne.ieu2022@student.ie.edu>
2024-10-13 22:52:24 +0200

    important changes to config.py

--- config.py
+++ config.py
@@ -1,20 +1,15 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    #SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
 
 class LocalConfig(Config):
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}' '''.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )'''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
     DEBUG = True
 
 class GithubCIConfig(Config):
